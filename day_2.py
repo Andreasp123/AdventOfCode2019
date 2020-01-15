546309de2fab3bdee02f79cb7ef89ae87d705c74
Andreas <persson.andreeas@gmail.com>
2020-01-15 21:27:06 +0100

    day2: wip

---
+++ day_2.py
@@ -0,0 +1,18 @@
+from builtins import int, open, enumerate, zip
+
+
+def read_input():
+    with open('input_day2.txt', 'r') as f:
+        return [int(line.rstrip()) for line in f.readlines()]
+
+def loop_opcodes():
+    intcodes = read_input()
+    copy = intcodes
+    position = 0
+    while position <= len(intcodes):
+        if intcodes[position] == 1:
+            intcodes[position +3] = intcodes[position + 1] + intcodes[position + 2]
+        if intcodes[position] == 2:
+            intcodes[position + 3] = intcodes[position + 1] * intcodes[position + 2]
+if __name__ == '__main__':
+    read_input()
